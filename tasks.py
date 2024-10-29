eccda0ca77e78bb2fd90c596c087e6af6eb8e3ff
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2024-10-29 00:45:20 -0400

    fix

--- tasks.py
+++ tasks.py
@@ -6,3 +6,8 @@ def autodoc(c):
     c.run(
         "typer rescuebox/main.py utils docs --name rescuebox --output docs/Reference.md"
     )
+
+
+@task
+def serve(c):
+    c.run("poetry run python -m src.rb-api.rb.api.main")
